807543e074c3bd59cb07702f487ff54e16926626
Atharva <atharvamhaske76@gmail.com>
2025-11-28 23:40:40 +0530

    added signup and in structs

--- internal/domain/models/users.go
+++ internal/domain/models/users.go
@@ -14,4 +14,37 @@ type User struct {
 	UpdatedAt time.Time          `json:"updated_at" bson:"updated_at"`
 }
 
+//signup and signin
 
+type SignUpRequest struct {
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required,min=8"`
+}
+
+type SignUpResponse struct {
+	ID    primitive.ObjectID `json:"id"`
+	Email string             `json:"string"`
+}
+
+type SignInRequest struct {
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required,min=8"`
+}
+
+type SignInResponse struct {
+	Token string `json:"token"`
+}
+
+//public user(no signup)
+
+type PublicUserResponse struct {
+	ID    primitive.ObjectID `json:"id"`
+	Email string             `json:"email"`
+}
+
+func (u *User) Public() *PublicUserResponse {
+	return &PublicUserResponse{
+		ID:    u.ID,
+		Email: u.Email,
+	}
+}
